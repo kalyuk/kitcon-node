cb17ac6fb59aca6284a56f7e785db784c0cf67b3
Dmitry Kalyuk <kalyukdo@gmail.com>
2020-11-27 20:08:20 +0300

    fix for jwt token

--- src/services/jwt.service.ts
+++ src/services/jwt.service.ts
@@ -11,6 +11,7 @@ export class JwtService {
     private privateKey: any;
     constructor() {
         if (fs.existsSync(path.join((global as any).ROOT_PATH, '..', 'keys', 'public.key'))) {
+            console.log
             this.publicKey = fs.readFileSync(
                 path.join((global as any).ROOT_PATH, '..', 'keys', 'public.key')
             );
